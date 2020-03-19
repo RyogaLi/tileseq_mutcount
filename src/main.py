bb63ed827e37e414b48963f2205a822f6ae489ec
ryogali <ryogali@hotmail.com>
2020-03-19 14:37:38 -0400

    added name to sh

--- src/main.py
+++ src/main.py
@@ -209,7 +209,7 @@ class fastq2counts(object):
 
             # submit job with main.py -r1 and -r2
             # run main.py with -r1 and -r2
-            cmd = f"python {self._main_path} -r1 {self._r1} -r2 {self._r2} -o {self._output} -p {args.param} --skip_alignment -log {args.log_level} -env {args.environment} -qual {args.quality} -min {args.min_cover} -at {args.at} -mt {args.mt}"
+            cmd = f"python {self._main_path} -n {args.name} -r1 {self._r1} -r2 {self._r2} -o {self._output} -p {args.param} --skip_alignment -log {args.log_level} -env {args.environment} -qual {args.quality} -min {args.min_cover} -at {args.at} -mt {args.mt}"
             job_list = []
             if args.environment == "BC2" or args.environment == "BC":
                 logging.info("Submitting mutation counts jobs to BC2...")
