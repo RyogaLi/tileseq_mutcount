15a76927ab0df6cc0ca5a9d8adc5752c58d9c4ba
ryogali <ryogali@hotmail.com>
2020-10-17 20:18:42 -0400

    increased bowtie cores to 8

--- TileSeqMut/alignment.py
+++ TileSeqMut/alignment.py
@@ -35,8 +35,8 @@ def align_main(ref, r1, r2, sam_path, shfile):
     r1_sam_file = os.path.join(sam_path, os.path.basename(r1).replace(".fastq.gz", ".sam"))
     r2_sam_file = os.path.join(sam_path, os.path.basename(r2).replace(".fastq.gz", ".sam"))
 
-    r1_cmd = f"bowtie2 --no-head --norc --no-sq --rdg 12,1 --rfg 12,1 --local -x {ref} -U {r1} -S {r1_sam_file}"
-    r2_cmd = f"bowtie2 --no-head --nofw --no-sq --rdg 12,1 --rfg 12,1 --local -x {ref} -U {r2} -S {r2_sam_file}"
+    r1_cmd = f"bowtie2 --no-head --norc --no-sq --rdg 12,1 --rfg 12,1 --local -p 8 -x {ref} -U {r1} -S {r1_sam_file}"
+    r2_cmd = f"bowtie2 --no-head --nofw --no-sq --rdg 12,1 --rfg 12,1 --local -p 8 -x {ref} -U {r2} -S {r2_sam_file}"
 
     with open(shfile, "w") as f:
         f.write(r1_cmd + "\n")
