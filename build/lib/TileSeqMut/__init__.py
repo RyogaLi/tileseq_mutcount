8066f6eca7f60ee29402c42e940be47e37b5a234
ryogali <roujia.li@mail.utoronto.ca>
2021-07-27 09:42:24 -0400

    omitting build dir

--- build/lib/TileSeqMut/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3.6
-
-# Author: Roujia Li
-# email: Roujia.li@mail.utoronto.ca
-from pkg_resources import get_distribution
-
-__version__ = get_distribution('TileSeqMut').version
